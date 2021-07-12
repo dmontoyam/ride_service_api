e57ec887f64d13ffe82706fbbd771cd19b6ab45f
dmontoyadtech <dmontoya@dtech.com.co>
2021-07-12 11:13:19 -0500

    Corrección prueba Rspec

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -3,9 +3,6 @@ RSpec.describe Driver, type: :model do
     it 'ensures email presence' do
       is_expected.to validate_presence_of(:email)
     end
-    it 'ensures license_plate presence' do
-      is_expected.to validate_presence_of(:license_plate)
-    end
     # Associations tests
   end
 end
